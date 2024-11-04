f86f820a4536e9f29764a364dedc810f1e2dfaeb
马雄伟 <xiongwei.ma@dyness-tech.com>
2024-11-04 19:54:46 +0800

    feat: 云平台配置

--- miniprogram/setting/setting.js
+++ miniprogram/setting/setting.js
@@ -1,6 +1,14 @@
-﻿module.exports = {
+﻿/*
+ * @Author: 马雄伟 xiongwei.ma@dyness-tech.com
+ * @Date: 2024-11-04 17:02:55
+ * @LastEditors: 马雄伟 xiongwei.ma@dyness-tech.com
+ * @LastEditTime: 2024-11-04 19:44:22
+ * @FilePath: \yzzt\miniprogram\setting\setting.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+module.exports = {
 	//### 环境相关 
-	CLOUD_ID: 'cloud1-3go08kosfd9d453c', //云服务id ,本地测试环境 
+	CLOUD_ID: 'yzzt-1gv2jtms28bc42e0', //云服务id ,本地测试环境 
 
 	// #### 版本信息 
 	VER: 'build 2023.10.01',
